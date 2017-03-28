f15c9b88d8b8035b986d6b85c0f045174d0b8022
Teddy Toussaint <toussaintteddy@gmail.com>
2017-03-29 00:27:35 +0200

    Indentation correction in index.php

--- ttous/index/index.php
+++ ttous/index/index.php
@@ -7,18 +7,19 @@
 
         <div class="content center-block">
             <h1>Welcome to my website !</h1>
-            <p class="text-center">I created this website to train myself in WAMP programming. The 
-            navigation links on the top of the page are the Practicle Works I made 
-            with the openclassrooms course about making websites with PHP.<br />
+            <p class="text-center">I created this website to train myself in
+            WAMP programming. The navigation links on the top of the page are
+            the Practicle Works I made with the openclassrooms course about
+            making websites with PHP.<br />
             You can find this course by clicking
             <a href="https://openclassrooms.com/courses/concevez-votre-site-web-avec-php-et-mysql">
-            here</a> if you are interested. It will take you less than 30 hours to
-            finish it if you already have some web programming skills (you must be 
-            experienced in HTML5 and CSS3 though).<br />
-            Please feel free to contact me if you have any remark/suggestion/
-            greetings (why not?) to say, it will be a pleasure to discuss with you ! 
-            To that end, you can click on the mail link at the bottom of any page.
-            </p><br />
+            here</a> if you are interested. It will take you less than 30 hours
+            to finish it if you already have some web programming skills (you
+            must be experienced in HTML5 and CSS3 though).<br />
+            Please feel free to contact me if you have any
+            remark/suggestion/greetings (why not?) to say, it will be a pleasure
+            to discuss with you ! To that end, you can click on the mail link at
+            the bottom of any page.</p><br />
             <p>Browse well ;-)</p>
         </div>
 
