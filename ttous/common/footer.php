dd0953736de8134fdd86b4c9557cc98b11bc8986
Teddy Toussaint <toussaintteddy@gmail.com>
2017-03-30 00:41:11 +0200

    Added Log out button (but not the file) and icons to the navbar

--- ttous/common/footer.php
+++ ttous/common/footer.php
@@ -2,7 +2,7 @@
     <div class="text-center center-block">
         <p class="txt-railway">- Copyright <a href="../common/about.php">Teddy TOUSSAINT</a> -</p>
         <a href="https://www.linkedin.com/in/teddy-toussaint"><i id="logo-linkedin" class="fa fa-linkedin fa-3x social"></i></a>
-        <a href="https://github.com/ttouss"><i id="logo-github" class="fa fa-github fa-3x social"></i></a>
+        <a href="https://github.com/ttous"><i id="logo-github" class="fa fa-github fa-3x social"></i></a>
         <a href="mailto:teddy.toussaint@insa-lyon.fr"><i id="logo-email" class="fa fa-envelope-o fa-3x social"></i></a>
     </div>
 </footer>
